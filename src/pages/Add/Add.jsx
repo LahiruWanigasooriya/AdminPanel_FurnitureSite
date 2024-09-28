5b3fb972b581ff3dd88b8d45811f679403d7ceb5
LahiruWanigasooriya <lahirunisaluor123@gmail.com>
2024-09-29 00:45:04 +0530

    design updated add.css

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -43,11 +43,21 @@ const Add = ({url}) => {
 toast.error(response.data.message) //display message when item not added successfully
     }
     }
+
+
     return (
         <div className='add'>
             <form className='flex-col' onSubmit={onSubmitHandler}>
                 <div className='add-img-upload flex-col'>
-                    <p>Upload Image</p>
+                    <p>Upload Image :</p>
+                    <label htmlFor='image'>
+                        <img src={image ? URL.createObjectURL(image) : assets.upload} alt='upload' />
+                    </label>
+                    <input onChange={(e) => setImage(e.target.files[0])} type='file' id='image' hidden required />
+                </div>
+
+                <div className='add-img-upload flex-col'>
+                    <p>Upload 3D Image :</p>
                     <label htmlFor='image'>
                         <img src={image ? URL.createObjectURL(image) : assets.upload} alt='upload' />
                     </label>
@@ -55,18 +65,18 @@ toast.error(response.data.message) //display message when item not added success
                 </div>
 
                 <div className='add-product-name flex-col'>
-                    <p>Product Name</p>
-                    <input onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Type Here' />
+                    <p>Product Name :</p>
+                    <input onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Type your item name here' required />
                 </div>
 
                 <div className='add-product-description flex-col'>
-                    <p>Product Description</p>
-                    <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder="Write Content Here" required></textarea>
+                    <p>Product Description :</p>
+                    <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder="Type your item description here" required></textarea>
                 </div>
 
                 <div className='add-category-price'>
                     <div className='add-category flex-col'>
-                        <p>Product Category</p>
+                        <p>Product Category :</p>
                         <select onChange={onChangeHandler} name='category'>
                             <option value="Table">Table</option>
                             <option value="Chair">Chair</option>
@@ -74,12 +84,12 @@ toast.error(response.data.message) //display message when item not added success
                         </select>
                     </div>
                     <div className='add-price flex-col'>
-                        <p>Product Price</p>
-                        <input onChange={onChangeHandler} value={data.price} type='number' name='price' placeholder='Rs 20,000' />
+                        <p>Product Price :</p>
+                        <input onChange={onChangeHandler} value={data.price} type='number' name='price' placeholder='Amount' /> <span className="currency-label">LKR</span>
                     </div>
                 </div>
 
-                <button type='submit' className='add-btn'>ADD</button>
+                <button type='submit' className='add-btn'>Add Item</button>
             </form>
         </div>
     );
